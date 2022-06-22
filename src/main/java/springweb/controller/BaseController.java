6dbb4c0ab822a9fd70a99788a05dabed7ecc7b67
Archon <qq349074225@live.com>
2022-06-22 13:30:57 +0800

    1

---
+++ src/main/java/springweb/controller/BaseController.java
@@ -0,0 +1,27 @@
+package springweb.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springweb.domain.BaseResponse;
+import springweb.util.BaseResponses;
+
+
+/**
+ * @author Archon  2022/6/22
+ * @since
+ */
+@RestController
+public class BaseController {
+
+    @Value("${spring.application.name}")
+    private String profile;
+
+    @Value("${spring.application.mykey}")
+    private String mykey;
+
+    @GetMapping("/base/profile")
+    private BaseResponse getProfile() {
+        return BaseResponses.ok(String.format("spring.application.name=%s, spring.application.mykey=%s", profile, mykey));
+    }
+}
